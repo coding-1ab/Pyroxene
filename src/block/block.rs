8cd89864b8df57dfe71c8e818fccd29403d1215b
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-07 00:30:59 +0900

    Cleanup

--- src/block/block.rs
+++ src/block/block.rs
@@ -1,11 +1,11 @@
 use crate::block::transaction::Transaction;
 
-pub struct Block_Header{
+pub struct BlockHeader {
     prev_hash: [u8; 32],
     nonce: u64,
     merkle_root: [u8; 32]
 }
 pub struct Block{
-    block_header: Block_Header,
+    block_header: BlockHeader,
     txs: Vec<Transaction>
 }
