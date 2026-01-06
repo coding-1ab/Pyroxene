e0aaccee36bf2322c40e07311629894a70a88049
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-07 01:08:50 +0900

    Cleanup

--- src/verify.rs
+++ src/verify.rs
@@ -1,48 +1,52 @@
-use rsa::pss::SigningKey;
-use rsa::signature::{Keypair, RandomizedSigner, Verifier};
-// 비밀키 공개키 받아서 저장을 하고 어떤 파일을 사인하고 검증하기
-use rsa::{RsaPrivateKey, RsaPublicKey};
+/// 비밀키 공개키 받아서 저장을 하고 어떤 파일을 사인하고 검증하기
+///
+
+use rsa::pkcs8::{DecodePrivateKey, EncodePrivateKey};
+use rsa::pkcs8::{LineEnding};
+use rsa::pss::{Signature, SigningKey, VerifyingKey};
+use rsa::signature::{RandomizedSigner, Verifier};
+use rsa::RsaPrivateKey;
 use sha2::Sha256;
-use std::fs;
-use rsa::pkcs8::{DecodePublicKey, LineEnding};
-use rsa::pkcs8::{DecodePrivateKey, EncodePrivateKey, EncodePublicKey};
 
-fn generate_keys(path: &str) {
-    let private_key = RsaPrivateKey::new(&mut rand::rng(), 2048).expect("Failed to generate a private key");
-    let public_key = RsaPublicKey::from(&private_key);
+pub fn generate_keys() -> RsaPrivateKey {
+    let private_key = RsaPrivateKey::new(
+        &mut rand::rng(),
+        2048
+    ).expect("Failed to generate a private key");
 
-    private_key.write_pkcs8_pem_file(format!("{}/private.pem", path), LineEnding::CRLF).expect("Failed to write private key");
-    public_key.write_public_key_pem_file(format!("{}/public.pem", path), LineEnding::CRLF).expect("Failed to write public key");
+    private_key
 }
 
-fn load_keys(path: &str) -> (RsaPrivateKey, RsaPublicKey) {
+pub fn save_key(path: &str, private_key: &RsaPrivateKey) {
+    private_key
+        .write_pkcs8_pem_file(format!("{}/private.pem", path), LineEnding::CRLF)
+        .expect("Failed to write private key");
+}
+
+pub fn load_keys(path: &str) -> RsaPrivateKey {
     let private_key = RsaPrivateKey::read_pkcs8_pem_file(format!("{}/private.pem", path)).unwrap();
-    let public_key = RsaPublicKey::read_public_key_pem_file(format!("{}/public.pem",path)).unwrap();
-
-    return (private_key, public_key);
+    private_key
 }
-fn verify_koi() {
-    generate_keys(".");
-    let (private_key, public_key) = load_keys(".");
 
-    let text = fs::read("test.txt").unwrap();
+pub fn sign_data(data: &[u8], key: &SigningKey<Sha256>) -> Signature {
+    let signed_message = key.sign_with_rng(&mut rand::rng(), data);
+    signed_message
+}
 
-    let signing_key = SigningKey::<Sha256>::new(private_key);
-
-    let verifying_key = signing_key.verifying_key();
-
-    let signed_message = signing_key.sign_with_rng(&mut rand::rng(), text.as_slice());
-    let check = verifying_key.verify(text.as_slice(), &signed_message).expect("Verification failed");
-
-    println!("Signature check: {:?}", check);
+pub fn verify_data(data: &[u8], signature: &Signature, key: VerifyingKey<Sha256>) {
+    key.verify(data, signature).expect("Verification failed");
 }
 
 #[cfg(test)]
 mod test {
-    use crate::verify::verify_koi;
+    use crate::verify::{generate_keys, sign_data, verify_data};
+    use rsa::pss::{SigningKey, VerifyingKey};
 
     #[test]
-    fn testVerify(){
-        verify_koi();
+    fn test_verify() {
+        let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10u8];
+        let key = generate_keys();
+        let signature = sign_data(&data, &SigningKey::new(key.clone()));
+        verify_data(&data, &signature, VerifyingKey::new(key.to_public_key()));
     }
 }
