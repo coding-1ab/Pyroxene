f5c1cbdbfb48f632c2a4be4b8d5dfcba8241a2ce
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-07 23:16:56 +0900

    네트워크 API 간편화

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -1,12 +1,16 @@
+use crate::network::protocol::Packet;
+use rkyv::rancor::Error;
+use rkyv::{from_bytes, to_bytes};
 use socket2::{Domain, Protocol, Socket, Type};
 use std::io::Result;
-use std::net::{SocketAddr, SocketAddrV4, UdpSocket};
+use std::net::{SocketAddr, UdpSocket};
 
 mod protocol;
 
 pub const PORT: u16 = 1200;
 
 pub struct UdpBroadcast {
+    buffer: Box<[u8; 1024]>,
     socket: UdpSocket,
     target: SocketAddr,
 }
@@ -27,52 +31,27 @@ impl UdpBroadcast {
         let socket: UdpSocket = socket.into();
 
         Ok(Self {
+            buffer: unsafe { Box::new_zeroed().assume_init() },
             socket,
             target: ([255, 255, 255, 255], send).into(), // 브로드캐스트 주소 바꿀 것
         })
     }
 
-    pub fn send(&self, data: &[u8]) -> Result<usize> {
-        self.socket.send_to(data, self.target)
+    pub fn send(&self, data: &Packet) -> Result<usize> {
+        self.socket.send_to(to_bytes::<Error>(data).unwrap().as_slice(), self.target)
     }
 
-    pub fn recv(&self, buf: &mut [u8]) -> Result<(usize, SocketAddr)> {
-        self.socket.recv_from(buf)
+    pub fn recv(&mut self) -> Result<Packet> {
+        let (length, _) = self.socket.recv_from(self.buffer.as_mut_slice())?;
+        if length == self.buffer.len() {
+            panic!("Buffer Overflow");
+        }
+
+        let contents = &self.buffer.as_slice()[..length];
+        Ok(from_bytes::<Packet, Error>(contents).unwrap())
     }
 
     pub fn is_self(&self, addr: SocketAddr) -> bool {
         addr == self.target
     }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::thread;
-    use std::time::Duration;
-    use crate::network::UdpBroadcast;
-
-    #[test]
-    fn test_broadcast() {
-        let node1 = UdpBroadcast::new().unwrap();
-        let node2 = UdpBroadcast::new().unwrap();
-        let data: [u8; 4] = [0x43, 0x55, 0x54, 0x45];
-        let mut receive_buffer = [0u8; 4];
-
-        let receiver = thread::scope(|scope| {
-            let result = scope.spawn(|| {
-                thread::sleep(Duration::from_secs(1));
-                node2.recv(&mut receive_buffer).unwrap()
-            });
-            scope.spawn(|| {
-                node1.send(&data).unwrap();
-            });
-            result.join().unwrap()
-        });
-        let (receive_count, sender_address) = receiver;
-
-        assert_eq!(receive_count, data.len());
-        assert_eq!(receive_buffer, data);
-
-    }
-
 }
--- src/network/protocol.rs
+++ src/network/protocol.rs
@@ -30,42 +30,30 @@ mod tests {
     use std::net::Ipv4Addr;
     use std::thread;
     use std::time::Duration;
-    use rkyv::from_bytes;
 
     #[test]
     fn test_packet_broadcast() {
         let node1 = UdpBroadcast::with_port(1201, 1201).unwrap();
-        let node2 = UdpBroadcast::with_port(1201, 1201).unwrap();
+        let mut node2 = UdpBroadcast::with_port(1201, 1201).unwrap();
 
         // 보낼 패킷
-        let packet = Packet::new(Ipv4Addr::new(192, 168, 0, 100));
+        let to_send = Packet::new(Ipv4Addr::new(192, 168, 0, 100));
 
-        let serialized = rkyv::to_bytes::<rkyv::rancor::Error>(&packet).unwrap();
-
-        let mut receive_buffer = [0u8; 1024]; // 1KB 버퍼 재사용
-
-        let receiver = thread::scope(|scope| {
+        let received = thread::scope(|scope| {
             let result = scope.spawn(|| {
                 thread::sleep(Duration::from_millis(100));
-                let (size, addr) = node2.recv(&mut receive_buffer).unwrap();
-                (size, addr)
+                let received = node2.recv().unwrap();
+                received
             });
 
             scope.spawn(|| {
                 thread::sleep(Duration::from_millis(50));
-                node1.send(&serialized).unwrap();
+                node1.send(&to_send).unwrap();
             });
 
             result.join().unwrap()
         });
 
-        let (received_size, _sender_addr) = receiver;
-
-
-        let valid_data = &receive_buffer[..received_size];
-
-        let archived = from_bytes::<Packet, rkyv::rancor::Error>(valid_data).unwrap();
-
-        assert_eq!(archived.sender(), packet.sender());
+        assert_eq!(to_send, received);
     }
 }
