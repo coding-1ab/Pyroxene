1bc3b15f8f87ad58a4ef0246c1e0198011e2f6fa
tmvkrpxl0 <tmvkrpxl0@proton.me>
2026-01-07 21:42:32 +0900

    테스트 고침

--- src/network.rs
+++ src/network.rs
@@ -5,25 +5,21 @@ use std::net::{SocketAddr, UdpSocket};
 pub struct UdpBroadcast {
     socket: UdpSocket,
     target: SocketAddr,
-    local: SocketAddr,
 }
 
 impl UdpBroadcast {
     pub fn new(port: u16) -> Result<Self> {
         let socket = Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
-        socket.set_reuse_address(true)?;
         socket.set_broadcast(true)?;
 
         let addr: SocketAddr = ([0, 0, 0, 0], port).into(); // 0.0.0.0 != 192.168.x.x.. 미래의 나, 해결해라!
         socket.bind(&addr.into())?;
 
         let socket: UdpSocket = socket.into();
-        let local = socket.local_addr()?;
 
         Ok(Self {
             socket,
             target: ([255, 255, 255, 255], port).into(), // 브로드캐스트 주소 바꿀 것
-            local,
         })
     }
 
@@ -35,25 +31,39 @@ impl UdpBroadcast {
         self.socket.recv_from(buf)
     }
 
-    pub fn local_addr(&self) -> SocketAddr {
-        self.local
-    }
-
     pub fn is_self(&self, addr: SocketAddr) -> bool {
-        addr == self.local
+        addr == self.target
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::sync::mpsc;
+    use std::thread;
+    use std::time::Duration;
+    use crate::network::UdpBroadcast;
+
     #[test]
     fn test_broadcast() {
-        let (sender, listener) = mpsc::channel();
+        let node1 = UdpBroadcast::new(8823).unwrap();
+        let node2 = UdpBroadcast::new(9265).unwrap();
         let data: [u8; 4] = [0x43, 0x55, 0x54, 0x45];
-        sender.send(data).unwrap();
-        let received = listener.recv().unwrap();
-        assert_eq!(received, data);
+        let mut receive_buffer = [0u8; 4];
+
+        let receiver = thread::scope(|scope| {
+            let result = scope.spawn(|| {
+                thread::sleep(Duration::from_secs(1));
+                node2.recv(&mut receive_buffer).unwrap()
+            });
+            scope.spawn(|| {
+                node1.send(&data).unwrap();
+            });
+            result.join().unwrap()
+        });
+        let (receive_count, sender_address) = receiver;
+
+        assert_eq!(receive_count, data.len());
+        assert_eq!(node1.target, sender_address);
+        assert_eq!(receive_buffer, data);
 
     }
 
