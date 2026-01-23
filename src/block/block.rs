ff402004541087762345fc2310efab7ab1549e2c
Unchainablemobb <scouttracer99@gmail.com>
2026-01-23 15:48:01 +0900

    id를 용어에 맞게 height(높이)로 수정

--- src/block/block.rs
+++ src/block/block.rs
@@ -5,7 +5,7 @@ use crate::block::Address;
 #[derive(Archive, Serialize, Deserialize, Debug, Clone)]
 pub struct BlockHeader {
     pub prev_hash: [u8; 32],
-    pub id: u64,
+    pub height: u64,
     pub nonce: u64,
     pub merkle_root: [u8; 32],
     pub nonce_founder: Address
