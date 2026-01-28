eec3ded4846640767ae92eac063050f2aaa11b23
Unchainablemobb <scouttracer99@gmail.com>
2026-01-28 22:07:34 +0900

    rocksdb로 db교체

--- src/database/trie.rs
+++ src/database/trie.rs
@@ -1,13 +1,25 @@
 //0<Nibble<0f
 pub type Nibble = u8;
 
+fn byte_to_nibble(bytes:&[u8])->Vec<Nibble>{
+    let ret:Vec<Nibble> = vec![];
+    for i in bytes{
+        
+    }
+}
+
 pub enum Node {
     Extension{
         nibble: Vec<Nibble>,
         child: Node
-    }
+    },
     Branch{
         children: [Node;16],
-
+        value: u128
+    },
+    Leaf{
+        key: [Nibble;64],
+        value: u128
     }
 }
+
